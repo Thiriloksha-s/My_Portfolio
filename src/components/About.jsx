089f065a794d022a647de5c3499bcf5d739a104a
Thiriloksha-s <thiriloksha@gmail.com>
2025-02-24 09:54:27 +0530

    Changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <section id="About" className='about-container'>
       <div className="about-content">
-        <img src={Picture} alt="Profile" className='about-image'/>
+        <img src={Picture} alt="Profile" className='about-image'/> 
         <div className="about-text">
           <p>
             I am an Electronics and Communication Engineering student with a strong analytical mindset and a passion for problem-solving. My journey in technology has led me to explore full-stack development, focusing on programming and cloud computing. I am passionate about continuous learning in data structures, algorithms, and backend development and enjoy collaborating on innovative projects.
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,10 +7,6 @@ const Home = () => {
       <div className="home-content">
         <div className="home-text">
           <h1 className="home-title">Hello!!, I'm Thiriloksha S</h1>
-          <p className="home-subtitle">
-            Full-Stack Enthusiast | Cloud Computing Explorer
-          </p>
-          
         </div>
         {/* <img src={Picture} alt="Profile" className="home-image" /> */}
       </div>
